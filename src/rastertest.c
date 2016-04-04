6527fb3b1c373c27d5a0c8a98c7c4ad36e02f47f
Marisa Kirisame <marisa@sayachan.org>
2016-04-05 00:45:53 +0200

    Have a spinning cube!

---
+++ src/rastertest.c
@@ -0,0 +1,332 @@
+/*
+	rastertest.c : Just learning.
+	This code is a mess, be warned.
+	(C)2016 Marisa Kirisame, UnSX Team.
+	Released under the GNU GPLv3 (or later).
+*/
+#define _DEFAULT_SOURCE
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include "rastertest.h"
+
+vect_t cubevect[8] =
+{
+	{-0.5f,-0.5f, 0.5f,1.f},{ 0.5f,-0.5f, 0.5f,1.f},
+	{-0.5f, 0.5f, 0.5f,1.f},{ 0.5f, 0.5f, 0.5f,1.f},
+	{-0.5f,-0.5f,-0.5f,1.f},{ 0.5f,-0.5f,-0.5f,1.f},
+	{-0.5f, 0.5f,-0.5f,1.f},{ 0.5f, 0.5f,-0.5f,1.f}
+};
+vect_t cubenorm[6] =
+{
+	{1.f,0.f,0.f,1.f},{-1.f,0.f,0.f,1.f},
+	{0.f,1.f,0.f,1.f},{0.f,-1.f,0.f,1.f},
+	{0.f,0.f,1.f,1.f},{0.f,0.f,-1.f,1.f}
+};
+coord_t cubecoord[4] =
+{
+	{0.f,0.f},{1.f,0.f},
+	{0.f,1.f},{1.f,1.f}
+};
+color_t cubecolor[8] =
+{
+	{0.f,0.f,0.f,1.f},
+	{1.f,0.f,0.f,1.f},
+	{0.f,1.f,0.f,1.f},
+	{1.f,1.f,0.f,1.f},
+	{0.f,0.f,1.f,1.f},
+	{1.f,0.f,1.f,1.f},
+	{0.f,1.f,1.f,1.f},
+	{1.f,1.f,1.f,1.f},
+};
+face_t cubefaces[12] =
+{
+	{{0,6,4},{0,0,0},{0,0,0},{0,6,4},0},
+	{{0,2,6},{0,0,0},{0,0,0},{0,2,6},0},
+	{{0,3,2},{0,0,0},{0,0,0},{0,3,2},0},
+	{{0,1,3},{0,0,0},{0,0,0},{0,1,3},0},
+	{{2,7,6},{0,0,0},{0,0,0},{2,7,6},0},
+	{{2,3,7},{0,0,0},{0,0,0},{2,3,7},0},
+	{{4,6,7},{0,0,0},{0,0,0},{4,6,7},0},
+	{{4,7,5},{0,0,0},{0,0,0},{4,7,5},0},
+	{{0,4,5},{0,0,0},{0,0,0},{0,4,5},0},
+	{{0,5,1},{0,0,0},{0,0,0},{0,5,1},0},
+	{{1,5,7},{0,0,0},{0,0,0},{1,5,7},0},
+	{{1,7,3},{0,0,0},{0,0,0},{1,7,3},0}
+};
+model_t cube =
+{
+	cubevect,cubenorm,cubecoord,cubecolor,0,cubefaces,8,6,4,8,0,12
+};
+vect_t cubepos = {0.f,0.f,-5.f,1.f};
+vect_t cuberot = {0.f,0.f,0.f,0.f};
+mat_t projection, translation, rotation;
+buffer_t screen = {0,0,0,640,480};
+
+/* FPS helper code */
+#define TMAX 64
+int ti = 0;
+float ts = 0.f, tl[TMAX] = {0.f};
+float avg_fps( float nt )
+{
+	ts = ts-tl[ti]+nt;
+	tl[ti] = nt;
+	if ( ++ti == TMAX ) ti = 0;
+	return ts/TMAX;
+}
+#define NANOS_SEC 1000000000L
+long ticker( void )
+{
+	struct timespec ts;
+	clock_gettime(CLOCK_MONOTONIC_RAW,&ts);
+	return ts.tv_nsec+ts.tv_sec*NANOS_SEC;
+}
+
+/* raster code */
+
+void putpixel( px_t p, color_t c )
+{
+	if ( (p.x < 0) || (p.x >= screen.width ) || (p.y < 0)
+		|| (p.y >= screen.height) )
+		return;
+	int coord = p.x+p.y*screen.width;
+	float dep = screen.depth[coord];
+	if ( (p.d < 0.f) || (p.d > dep) ) return;
+	screen.depth[coord] = p.d;
+	screen.color[coord].r = c.r*255.f;
+	screen.color[coord].g = c.g*255.f;
+	screen.color[coord].b = c.b*255.f;
+	screen.color[coord].a = c.a*255.f;
+}
+
+void drawline( int x0, int x1, int y0, int y1, color_t c )
+{
+	int steep = 0;
+	if ( abs(x0-x1) < abs(y0-y1) )
+	{
+		swap(int,x0,y0);
+		swap(int,x1,y1);
+		steep = 1;
+	}
+	if ( x0 > x1 )
+	{
+		swap(int,x0,x1);
+		swap(int,y0,y1);
+	}
+	int dx = x1-x0;
+	int dy = y1-y0;
+	int der = abs(dy)*2;
+	int er = 0;
+	int y = y0;
+	for ( int x=x0; x<=x1; x++ )
+	{
+		px_t p = {x,y,0.f,0};
+		if ( steep ) swap(int,p.x,p.y);
+		putpixel(p,c);
+		er += der;
+		if ( er > dx )
+		{
+			y += (y1>y0)?1:-1;
+			er -= dx*2;
+		}
+	}
+}
+
+void filltriangle( px_t p[3], color_t c[3] )
+{
+	px_t minb, maxb;
+	maxb.x = clamp(max(p[0].x,max(p[1].x,p[2].x)),0,screen.width);
+	maxb.y = clamp(max(p[0].y,max(p[1].y,p[2].y)),0,screen.height);
+	minb.x = clamp(min(p[0].x,min(p[1].x,p[2].x)),0,screen.width);
+	minb.y = clamp(min(p[0].y,min(p[1].y,p[2].y)),0,screen.height);
+	vect_t d1 = {p[1].x-p[0].x,p[1].y-p[0].y,0.f,0.f},
+		d2 = {p[2].x-p[0].x,p[2].y-p[0].y,0.f,0.f};
+	color_t c1 = {c[1].r-c[0].r,c[1].g-c[0].g,c[1].b-c[0].b,c[1].a-c[0].a},
+		c2 = {c[2].r-c[0].r,c[2].g-c[0].g,c[2].b-c[0].b,c[2].a-c[0].a};
+	float dp1 = p[1].d-p[0].d, dp2 = p[2].d-p[0].d;
+	#pragma omp parallel for
+	for ( int y=minb.y; y<maxb.y; y++ )
+	{
+		for ( int x=minb.x; x<maxb.x; x++ )
+		{
+			vect_t q = {x-p[0].x,y-p[0].y,0.f,0.f};
+			float u = d1.x*d2.y-d1.y*d2.x,
+				s = (q.x*d2.y-q.y*d2.x)/u,
+				t = (d1.x*q.y-d1.y*q.x)/u;
+			if ( (s>=0.f) && (t>=0.f) && (s+t<=1.f) )
+			{
+				color_t pc =
+				{
+					c[0].r+s*c1.r+t*c2.r,
+					c[0].g+s*c1.g+t*c2.g,
+					c[0].b+s*c1.b+t*c2.b,
+					c[0].a+s*c1.a+t*c2.a
+				};
+				px_t px = {x,y,p[0].d+s*dp1+t*dp2,0};
+				putpixel(px,pc);
+			}
+		}
+	}
+}
+
+void drawtriangle( tri_t t, unsigned char wireframe )
+{
+	px_t pts[3] =
+	{
+		{floorf((t.v[0].x*0.5f+0.5f)*screen.width),
+			floorf((t.v[0].y*0.5f+0.5f)*screen.height),-t.v[0].z,
+			0.f},
+		{floorf((t.v[1].x*0.5f+0.5f)*screen.width),
+			floorf((t.v[1].y*0.5f+0.5f)*screen.height),-t.v[1].z,
+			0.f},
+		{floorf((t.v[2].x*0.5f+0.5f)*screen.width),
+			floorf((t.v[2].y*0.5f+0.5f)*screen.height),-t.v[2].z,
+			0.f},
+	};
+	if ( wireframe )
+	{
+		color_t wire = {0.f,0.6f,0.f,1.f};
+		drawline(pts[0].x,pts[1].x,pts[0].y,pts[1].y,wire);
+		drawline(pts[0].x,pts[2].x,pts[0].y,pts[2].y,wire);
+		drawline(pts[1].x,pts[2].x,pts[1].y,pts[2].y,wire);
+		return;
+	}
+	vect_t facet, ab, ac;
+	ab.x = t.v[1].x-t.v[0].x;
+	ab.y = t.v[1].y-t.v[0].y;
+	ab.z = t.v[1].z-t.v[0].z;
+	ac.x = t.v[2].x-t.v[0].x;
+	ac.y = t.v[2].y-t.v[0].y;
+	ac.z = t.v[2].z-t.v[0].z;
+	cross(&facet,ab,ac);
+	if ( facet.z < 0.f ) return;
+	color_t cols[3] = {t.c[0],t.c[1],t.c[2]};
+	filltriangle(pts,cols);
+}
+
+pixel_t clearcolor = {16,16,16,255};
+float cleardepth = INFINITY;
+
+void rendermodel( void )
+{
+	int i;
+	for ( i=0; i<screen.width*screen.height; i++ )
+	{
+		screen.color[i] = clearcolor;
+		screen.depth[i] = cleardepth;
+	}
+	mat_t fulltransform;
+	vect_t basev[3], transv[3];
+	fulltransform = rotation;
+	mmul(&fulltransform,fulltransform,translation);
+	mmul(&fulltransform,fulltransform,projection);
+	for ( int i=0; i<cube.ntri; i++ )
+	{
+		#pragma omp parallel for
+		for ( int j=0; j<3; j++ )
+		{
+			basev[j].x = cube.vertices[cube.triangles[i].v[j]].x;
+			basev[j].y = cube.vertices[cube.triangles[i].v[j]].y;
+			basev[j].z = cube.vertices[cube.triangles[i].v[j]].z;
+			basev[j].w = 1.f;
+			vmat(&transv[j],fulltransform,basev[j]);
+			transv[j].x /= transv[j].w;
+			transv[j].y /= transv[j].w;
+			transv[j].z /= transv[j].w;
+		}
+		tri_t face =
+		{
+			{transv[0],transv[1],transv[2]},
+			{{0,0,0,0},{0,0,0,0},{0,0,0,0}},
+			{{0,0},{0,0},{0,0}},
+			{cube.colors[cube.triangles[i].c[0]],
+			cube.colors[cube.triangles[i].c[1]],
+			cube.colors[cube.triangles[i].c[2]]},
+			0
+		};
+		drawtriangle(face,0);
+	}
+}
+
+int main( void )
+{
+	SDL_Init(SDL_INIT_VIDEO|SDL_INIT_EVENTS);
+	SDL_Window *win = SDL_CreateWindow("RasterTest",SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,screen.width,screen.height,
+		SDL_WINDOW_SHOWN);
+	SDL_Surface *scr = SDL_GetWindowSurface(win);
+	screen.color = malloc(sizeof(pixel_t)*screen.width*screen.height);
+	screen.depth = malloc(sizeof(float)*screen.width*screen.height);
+	SDL_Surface *fb = SDL_CreateRGBSurfaceFrom(screen.color,screen.width,
+		screen.height,32,sizeof(pixel_t)*screen.width,0xFF0000,0xFF00,
+		0xFF,0xFF000000);
+	TTF_Init();
+	TTF_Font *fon = TTF_OpenFont("/usr/share/fonts/misc/unifont.bdf",16);
+	SDL_Event e;
+	SDL_Color fpscol = {160,0,0,255};
+	int active = 1;
+	float t = 0.f;
+	float frame = 0.f, fps = NAN, fpsmin = INFINITY, fpsmax = -INFINITY,
+		fpsavg = 0.f;
+	long tick, tock;
+	float znear = 0.1f, zfar = 100.f;
+	float fh = tanf(90.f/360.f*PI)*znear,
+		fw = fh*(screen.width/(float)screen.height);
+	frustum(&projection,-fw,fw,-fh,fh,znear,zfar);
+	char fpst[16];
+	while ( active )
+	{
+		tick = ticker();
+		while ( SDL_PollEvent(&e) )
+		{	if ( e.type == SDL_QUIT ) active = 0;
+			if ( e.type == SDL_KEYDOWN )
+			{
+				if ( e.key.keysym.sym == SDLK_ESCAPE )
+					active = 0;
+				else if ( e.key.keysym.sym == SDLK_a )
+					cubepos.x -= 0.1f;
+				else if ( e.key.keysym.sym == SDLK_d )
+					cubepos.x += 0.1f;
+				else if ( e.key.keysym.sym == SDLK_q )
+					cubepos.y -= 0.1f;
+				else if ( e.key.keysym.sym == SDLK_e )
+					cubepos.y += 0.1f;
+				else if ( e.key.keysym.sym == SDLK_w )
+					cubepos.z += 0.1f;
+				else if ( e.key.keysym.sym == SDLK_s )
+					cubepos.z -= 0.1f;
+			}
+		}
+		mat_t rotx, roty;
+		rotate(&rotx,50.f*t,ROT_X);
+		rotate(&roty,80.f*t,ROT_Y);
+		mmul(&rotation,rotx,roty);
+		translate(&translation,cubepos);
+		rendermodel();
+		SDL_BlitSurface(fb,0,scr,0);
+		snprintf(fpst,15,"%.2f FPS",fps);
+		SDL_Surface *txt = TTF_RenderText_Blended(fon,fpst,fpscol);
+		SDL_BlitSurface(txt,0,scr,0);
+		SDL_FreeSurface(txt);
+		SDL_UpdateWindowSurface(win);
+		tock = ticker();
+		frame = (float)(tock-tick)/NANOS_SEC;
+		fps = 1.f/frame;
+		fpsavg = avg_fps(fps);
+		if ( fps > fpsmax ) fpsmax = fps;
+		if ( fps < fpsmin ) fpsmin = fps;
+		t += frame;
+	}
+	free(screen.color);
+	free(screen.depth);
+	TTF_CloseFont(fon);
+	TTF_Quit();
+	SDL_FreeSurface(fb);
+	SDL_DestroyWindow(win);
+	SDL_Quit();
+	printf("FPS: %.2f min, %.2f max, %.2f avg\n",fpsmin,fpsmax,fpsavg);
+	return 0;
+}
---
+++ src/rastertest.h
@@ -0,0 +1,304 @@
+/*
+	rastertest.h : Just learning.
+	This code is a mess, be warned.
+	(C)2016 Marisa Kirisame, UnSX Team.
+	Released under the GNU GPLv3 (or later).
+*/
+typedef struct
+{
+	float x,y,z,w;
+} vect_t;
+typedef struct
+{
+	float s,t;
+} coord_t;
+typedef struct
+{
+	float r,g,b,a;
+} color_t;
+typedef struct
+{
+	float c[4][4];
+} mat_t;
+typedef struct
+{
+	int x,y;
+	float d;
+	int s;
+} px_t;
+typedef struct
+{
+	unsigned char b,g,r,a;
+} __attribute__((packed)) pixel_t;
+typedef struct
+{
+	int v[3], n[3], t[3], c[3], mat;
+} face_t;
+typedef struct
+{
+	pixel_t *color;
+	float *depth;
+	int *stencil;
+	int width, height;
+} buffer_t;
+#define TXCLAMP  0
+#define TXWRAP   1
+#define TXMIRROR 2
+#define TXBORDER 3
+typedef struct
+{
+	pixel_t *data;
+	int width, height, depth;
+	int bu, bv;
+	color_t border;
+} texture_t;
+/*
+   material reference:
+    diffuse: rgba color
+    normal: rgb tangent, a parallax
+    lighting: r roughness, g metalness, b glowness, a unused
+    cubemap: rgba color
+*/
+#define BLNONE  0
+#define BLALPHA 1
+#define BLADD   2
+#define BLMUL   3
+#define BLSUB   4
+#define BLDIV   5
+#define BLOVER  6
+#define BLMOD   7
+typedef struct
+{
+	texture_t *diffuse, *normal, *lighting, *cubemap;
+	color_t diffmul, lightmul;
+	int blend;
+} material_t;
+typedef struct
+{
+	vect_t v[3], n[3];
+	coord_t t[3];
+	color_t c[3];
+	material_t *m;
+} tri_t;
+typedef struct
+{
+	vect_t *vertices;
+	vect_t *normals;
+	coord_t *txcoords;
+	color_t *colors;
+	material_t *materials;
+	face_t *triangles;
+	int nvert, nnorm, ncoord, ncolor, nmat, ntri;
+} model_t;
+void vadd( vect_t *o, vect_t a, vect_t b )
+{
+#ifdef __SSE__
+	__asm("movups %1,%%xmm0\naddps %2,%%xmm0\nmovups %%xmm0,%0\n"
+		:"=m"(o):"m"(a),"m"(b));
+#else
+	o->x = a.x+b.x;
+	o->y = a.y+b.y;
+	o->z = a.z+b.z;
+	o->w = a.w+b.w;
+#endif
+}
+void vmul( vect_t *o, vect_t a, vect_t b )
+{
+#ifdef __SSE__
+	__asm("movups %1,%%xmm0\nmulps %2,%%xmm0\nmovups %%xmm0,%0\n"
+		:"=m"(o):"m"(a),"m"(b));
+#else
+	o->x = a.x*b.x;
+	o->y = a.y*b.y;
+	o->z = a.z*b.z;
+	o->w = a.w*b.w;
+#endif
+}
+void vscale( vect_t *o, vect_t a, float b )
+{
+#ifdef __SSE__
+	vect_t bv = {b,b,b,b};
+	__asm("movups %1,%%xmm0\nmulps %2,%%xmm0\nmovups %%xmm0,%0\n"
+		:"=m"(o):"m"(a),"m"(bv));
+#else
+	o->x = a.x*b;
+	o->y = a.y*b;
+	o->z = a.z*b;
+	o->w = a.w*b;
+#endif
+}
+void vmat( vect_t *o, mat_t a, vect_t b )
+{
+	o->x = a.c[0][0]*b.x+a.c[1][0]*b.y+a.c[2][0]*b.z+a.c[3][0]*b.w;
+	o->y = a.c[0][1]*b.x+a.c[1][1]*b.y+a.c[2][1]*b.z+a.c[3][1]*b.w;
+	o->z = a.c[0][2]*b.x+a.c[1][2]*b.y+a.c[2][2]*b.z+a.c[3][2]*b.w;
+	o->w = a.c[0][3]*b.x+a.c[1][3]*b.y+a.c[2][3]*b.z+a.c[3][3]*b.w;
+}
+void mident( mat_t *o )
+{
+	int i,j;
+	for ( i=0; i<4; i++ ) for ( j=0; j<4; j++ )
+		o->c[i][j] = (i==j)?1.f:0.f;
+}
+void mmul( mat_t *o, mat_t a, mat_t b )
+{
+	int i,j;
+	for ( i=0; i<4; i++ ) for ( j=0; j<4; j++ )
+		o->c[i][j] = a.c[i][0]*b.c[0][j]+a.c[i][1]*b.c[1][j]
+			+a.c[i][2]*b.c[2][j]+a.c[i][3]*b.c[3][j];
+}
+void mscale( mat_t *o, mat_t a, float b )
+{
+	int i,j;
+	for ( i=0; i<4; i++ ) for ( j=0; j<4; j++ )
+		o->c[i][j] = a.c[i][j]*b;
+}
+void cadd( color_t *o, color_t a, color_t b )
+{
+#ifdef __SSE__
+	__asm("movups %1,%%xmm0\naddps %2,%%xmm0\nmovups %%xmm0,%0\n"
+		:"=m"(o):"m"(a),"m"(b));
+#else
+	o->r = a.r+b.r;
+	o->g = a.g+b.g;
+	o->b = a.b+b.b;
+	o->a = a.a+b.a;
+#endif
+}
+void cmul( color_t *o, color_t a, color_t b )
+{
+#ifdef __SSE__
+	__asm("movups %1,%%xmm0\nmulps %2,%%xmm0\nmovups %%xmm0,%0\n"
+		:"=m"(o):"m"(a),"m"(b));
+#else
+	o->r = a.r*b.r;
+	o->g = a.g*b.g;
+	o->b = a.b*b.b;
+	o->a = a.a*b.a;
+#endif
+}
+void cscale( color_t *o, color_t a, float b )
+{
+#ifdef __SSE__
+	color_t bv = {b,b,b,b};
+	__asm("movups %1,%%xmm0\nmulps %2,%%xmm0\nmovups %%xmm0,%0\n"
+		:"=m"(o):"m"(a),"m"(bv));
+#else
+	o->r = a.r*b;
+	o->g = a.g*b;
+	o->b = a.b*b;
+	o->a = a.a*b;
+#endif
+}
+#define PI 3.14159265359f
+void frustum( mat_t *o, float left, float right, float bottom, float top,
+	float near, float far )
+{
+	o->c[0][0] = (2.f*near)/(right-left);
+	o->c[0][1] = 0.f;
+	o->c[0][2] = (right+left)/(right-left);
+	o->c[0][3] = 0.f;
+	o->c[1][0] = 0.f;
+	o->c[1][1] = (2.f*near)/(top-bottom);
+	o->c[1][2] = (top+bottom)/(top-bottom);
+	o->c[1][3] = 0.f;
+	o->c[2][0] = 0.f;
+	o->c[2][1] = 0.f;
+	o->c[2][2] = (far+near)/(far-near);
+	o->c[2][3] = -(2.f*far*near)/(far-near);
+	o->c[3][0] = 0.f;
+	o->c[3][1] = 0.f;
+	o->c[3][2] = 1.f;
+	o->c[3][3] = 0.f;
+}
+#define ROT_X 0
+#define ROT_Y 1
+#define ROT_Z 2
+void rotate( mat_t *o, float angle, int axis )
+{
+	float theta = (angle/180.f)*PI;
+	vect_t n = {0.f,0.f,0.f,0.f};
+	if ( axis == 0 ) n.x = 1.f;
+	else if ( axis == 1 ) n.y = 1.f;
+	else n.z = 1.f;
+	float s = sinf(theta);
+	float c = cosf(theta);
+	float oc = 1.f-c;
+	o->c[0][0] = oc*n.x*n.x+c;
+	o->c[0][1] = oc*n.x*n.y-n.z*s;
+	o->c[0][2] = oc*n.z*n.x+n.y*s;
+	o->c[0][3] = 0.f;
+	o->c[1][0] = oc*n.x*n.y+n.z*s;
+	o->c[1][1] = oc*n.y*n.y+c;
+	o->c[1][2] = oc*n.y*n.z-n.x*s;
+	o->c[1][3] = 0.f;
+	o->c[2][0] = oc*n.z*n.x-n.y*s;
+	o->c[2][1] = oc*n.y*n.z+n.x*s;
+	o->c[2][2] = oc*n.z*n.z+c;
+	o->c[2][3] = 0.f;
+	o->c[3][0] = 0.f;
+	o->c[3][1] = 0.f;
+	o->c[3][2] = 0.f;
+	o->c[3][3] = 1.f;
+}
+void translate( mat_t *o, vect_t offset )
+{
+	o->c[0][0] = 1.f;
+	o->c[0][1] = 0.f;
+	o->c[0][2] = 0.f;
+	o->c[0][3] = 0.f;
+	o->c[1][0] = 0.f;
+	o->c[1][1] = 1.f;
+	o->c[1][2] = 0.f;
+	o->c[1][3] = 0.f;
+	o->c[2][0] = 0.f;
+	o->c[2][1] = 0.f;
+	o->c[2][2] = 1.f;
+	o->c[2][3] = 0.f;
+	o->c[3][0] = offset.x;
+	o->c[3][1] = offset.y;
+	o->c[3][2] = offset.z;
+	o->c[3][3] = 1.f;
+}
+#define saturate(x) (((x)>0)?((x)>1)?1:(x):0)
+#define swap(t,x,y) {t tmp=x;x=y;y=tmp;}
+#define abs(x) (((x)>0)?(x):-(x))
+#define min(a,b) (((a)>(b))?(b):(a))
+#define max(a,b) (((a)>(b))?(a):(b))
+#define clamp(a,b,c) (((a)>(b))?((a)>(c))?(c):(a):(b))
+#define lerp(a,b,f) ((a)*(1.f-(f))+(b)*(f))
+void vlerp( vect_t *o, vect_t a, vect_t b, float f )
+{
+	o->x = lerp(a.x,b.x,f);
+	o->y = lerp(a.y,b.y,f);
+	o->z = lerp(a.z,b.z,f);
+	o->w = lerp(a.w,b.w,f);
+}
+void clerp( color_t *o, color_t a, color_t b, float f )
+{
+	o->r = lerp(a.r,b.r,f);
+	o->g = lerp(a.g,b.g,f);
+	o->b = lerp(a.b,b.b,f);
+	o->a = lerp(a.a,b.a,f);
+}
+float vsize( vect_t v )
+{
+	return sqrt(powf(v.x,2.f)+powf(v.y,2.f)+powf(v.z,2.f));
+}
+void cross( vect_t *a, vect_t b, vect_t c )
+{
+	a->x = b.y*c.z-b.z*c.y;
+	a->y = b.z*c.x-b.x*c.z;
+	a->z = b.x*c.y-b.y*c.x;
+}
+float dot( vect_t a, vect_t b )
+{
+	return a.x*b.x+a.y*b.y+a.z*b.z;
+}
+void normalize( vect_t *a )
+{
+	float scale = sqrtf(powf(a->x,2.f)+powf(a->y,2.f)+powf(a->z,2.f));
+	a->x /= scale;
+	a->y /= scale;
+	a->z /= scale;
+}
